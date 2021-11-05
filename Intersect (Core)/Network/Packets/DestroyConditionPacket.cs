1d0d03a740478aa4add4019df2ef7dfb4153b18c
AlexVild <ajv2324@gmail.com>
2021-11-05 09:18:55 -0400

    Alex: Add files I forgot to stage lol whoops

---
+++ Intersect (Core)/Network/Packets/DestroyConditionPacket.cs
@@ -0,0 +1,28 @@
+﻿using MessagePack;
+
+namespace Intersect.Network.Packets.Server
+{
+    [MessagePackObject]
+    public class DestroyConditionPacket : IntersectPacket
+    {
+        //Parameterless Constructor for MessagePack
+        public DestroyConditionPacket()
+        {
+            CanDestroy = false;
+            Index = 0;
+        }
+
+        public DestroyConditionPacket(bool canDestroy, int index)
+        {
+            CanDestroy = canDestroy;
+            Index = index;
+        }
+
+        [Key(0)]
+        public bool CanDestroy;
+
+        [Key(1)]
+        public int Index;
+    }
+
+}
---
+++ Intersect (Core)/Network/Packets/DestroyItemPacket.cs
@@ -0,0 +1,27 @@
+﻿using MessagePack;
+
+namespace Intersect.Network.Packets.Client
+{
+    [MessagePackObject]
+    public class DestroyItemPacket : SlotQuantityPacket
+    {
+        //Parameterless Constructor for MessagePack
+        public DestroyItemPacket()
+        {
+            Slot = 0;
+            Quantity = 0;
+            CheckCanDrop = false;
+        }
+
+        public DestroyItemPacket(int slot, int quantity, bool checkCanDrop)
+        {
+            Slot = slot;
+            Quantity = quantity;
+            CheckCanDrop = checkCanDrop;
+        }
+
+        [Key(4)]
+        public bool CheckCanDrop;
+    }
+
+}

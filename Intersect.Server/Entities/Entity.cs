5811e40b8c83d22740a3d22d0a56f270afda50be
AlexVild <ajv2324@gmail.com>
2022-01-09 11:43:37 -0500

    Alex: Fix issue where a player wouldn't leave a map they were warping to when changing instances

--- Intersect.Server/Entities/Entity.cs
+++ Intersect.Server/Entities/Entity.cs
@@ -35,6 +35,8 @@ namespace Intersect.Server.Entities
 
         public Guid InstanceLayer = Guid.Empty;
 
+        public Guid LastInstanceLayer = Guid.Empty;
+
         [JsonProperty("MaxVitals"), NotMapped] private int[] _maxVital = new int[(int) Vitals.VitalCount];
 
         [NotMapped, JsonIgnore] public Stat[] Stat = new Stat[(int) Stats.StatCount];
@@ -56,6 +58,7 @@ namespace Intersect.Server.Entities
                 }
             }
             InstanceLayer = instanceLayer;
+            LastInstanceLayer = InstanceLayer;
 
             Id = instanceId;
         }
@@ -346,6 +349,8 @@ namespace Intersect.Server.Entities
                     {
                         status.TryRemoveStatus();
                     }
+
+                    LastInstanceLayer = InstanceLayer;
                 }
             }
             finally
--- Intersect.Server/Maps/MapInstance.cs
+++ Intersect.Server/Maps/MapInstance.cs
@@ -27,7 +27,7 @@ namespace Intersect.Server.Maps
 
     public partial class MapInstance : MapBase
     {
-        private Dictionary<Guid, MapProcessingLayer> mMapProcessingLayers = new Dictionary<Guid, MapProcessingLayer>();
+        private ConcurrentDictionary<Guid, MapProcessingLayer> mMapProcessingLayers = new ConcurrentDictionary<Guid, MapProcessingLayer>();
 
         private static MapInstances sLookup;
 
@@ -1007,7 +1007,7 @@ namespace Intersect.Server.Maps
         public ICollection<Player> GetPlayersOnAllLayers()
         {
             var players = new List<Player>();
-            foreach (var layer in mMapProcessingLayers.Keys)
+            foreach (var layer in mMapProcessingLayers.Keys.ToList())
             {
                 players.AddRange(mMapProcessingLayers[layer].GetPlayersOnMap());
             }
@@ -1195,25 +1195,32 @@ namespace Intersect.Server.Maps
             return locations;
         }
 
-        public void CreateProcessingInstance(Guid processingLayerId)
+        /// <summary>
+        /// Creates a processing layer
+        /// </summary>
+        /// <param name="processingLayerId"></param>
+        /// <returns>Whether or not we needed to create a new processing layer</returns>
+        public bool CreateProcessingInstance(Guid processingLayerId)
         {
             if (!mMapProcessingLayers.ContainsKey(processingLayerId))
             {
+                Log.Debug($"Creating new processing layer {processingLayerId} for map {Name}");
                 mMapProcessingLayers[processingLayerId] = new MapProcessingLayer(this, processingLayerId);
-                mMapProcessingLayers[processingLayerId].Initialize();
+                return true;
             }
+            
+            return false;
         }
 
         public void UpdateProcessingInstances(long timeMs)
         {
-            RemoveDeadProcessingLayers();
-            foreach (var instance in mMapProcessingLayers.Keys)
+            foreach (var instance in mMapProcessingLayers.Keys.ToList())
             {
                 mMapProcessingLayers[instance].Update(timeMs);
             }
         }
 
-        public MapProcessingLayer GetRelevantProcessingLayer(Guid instanceLayer)
+        public MapProcessingLayer GetRelevantProcessingLayer(Guid instanceLayer, bool createIfNew = true)
         {
             if (mMapProcessingLayers.TryGetValue(instanceLayer, out var processingLayer))
             {
@@ -1221,8 +1228,19 @@ namespace Intersect.Server.Maps
             }
             else
             {
-                CreateProcessingInstance(instanceLayer);
-                return mMapProcessingLayers[instanceLayer];
+                if (createIfNew)
+                {
+                    if (CreateProcessingInstance(instanceLayer))
+                    {
+                        return mMapProcessingLayers[instanceLayer];
+                    } else
+                    {
+                        return null;
+                    }
+                } else
+                {
+                    return null;
+                }
             }
         }
 
@@ -1231,10 +1249,20 @@ namespace Intersect.Server.Maps
             lock (GetMapLock())
             {
                 // Removes all processing layers that don't have active players on themselves or any adjoining layers
-                foreach (var instance in mMapProcessingLayers.Where(kv => kv.Value.GetAllRelevantPlayers().Count <= 0).ToList())
+                var deadLayers = mMapProcessingLayers.Where(kv => kv.Value.GetAllRelevantPlayers().Count <= 0).ToList();
+                var layerCountPreCleanup = mMapProcessingLayers.Keys.Count;
+                foreach (var instance in deadLayers)
                 {
-                    Log.Debug($"Cleaning up MPL {instance}");
-                    mMapProcessingLayers.Remove(instance.Key);
+                    if (mMapProcessingLayers.TryRemove(instance.Key, out var removedLayer))
+                    {
+                        removedLayer.Dispose();
+                        Log.Debug($"Cleaning up MPL {instance} for map: {Name}");
+                    }
+                }
+
+                if (layerCountPreCleanup != mMapProcessingLayers.Keys.Count)
+                {
+                    Log.Debug($"There are now {mMapProcessingLayers.Keys.Count} layer(s) remaining for map: {Name}");
                 }
             }
         }

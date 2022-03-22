AscensionGameDev/Intersect-Engine

Swaps Globals.System.GetTimeMs() for Timing.Global.Milliseconds. (#941)
* Removed Client's MonoSystem.cs and Editor's Sys.cs (not used anymore).
* Removed unused overrides.
* Removed unused imports within the modified files.

Intersect.Client/Interface/Game/Hotbar/HotbarItem.cs
@@ -2,9 +2,7 @@
 
 using Intersect.Client.Core;
 using Intersect.Client.Core.Controls;
-using Intersect.Client.Framework.File_Management;
 using Intersect.Client.Framework.GenericClasses;
-using Intersect.Client.Framework.Graphics;
 using Intersect.Client.Framework.Gwen.Control;
 using Intersect.Client.Framework.Gwen.Control.EventArguments;
 using Intersect.Client.Framework.Gwen.Input;
@@ -14,6 +12,7 @@ using Intersect.Client.Items;
 using Intersect.Client.Localization;
 using Intersect.Client.Spells;
 using Intersect.GameObjects;
+using Intersect.Utilities;
 
 namespace Intersect.Client.Interface.Game.Hotbar
 {
@@ -136,7 +135,7 @@ namespace Intersect.Client.Interface.Game.Hotbar
 
         void pnl_Clicked(Base sender, ClickedEventArgs arguments)
         {
-            mClickTime = Globals.System.GetTimeMs() + 500;
+            mClickTime = Timing.Global.Milliseconds + 500;
         }
 
         void pnl_HoverLeave(Base sender, EventArgs arguments)
@@ -318,14 +317,14 @@ namespace Intersect.Client.Interface.Game.Hotbar
 
                 //Spell on cd
                 if (mSpellBookItem != null &&
-                    Globals.Me.GetSpellCooldown(mSpellBookItem.Id) > Globals.System.GetTimeMs())
+                    Globals.Me.GetSpellCooldown(mSpellBookItem.Id) > Timing.Global.Milliseconds)
                 {
                     updateDisplay = true;
                 }
 
                 //Spell on cd and the fade is incorrect
                 if (mSpellBookItem != null &&
-                    Globals.Me.GetSpellCooldown(mSpellBookItem.Id) > Globals.System.GetTimeMs() != mIsFaded)
+                    Globals.Me.GetSpellCooldown(mSpellBookItem.Id) > Timing.Global.Milliseconds != mIsFaded)
                 {
                     updateDisplay = true;
                 }
@@ -387,13 +386,13 @@ namespace Intersect.Client.Interface.Game.Hotbar
                     mCooldownLabel.IsHidden = true;
                     if (mSpellBookItem != null)
                     {
-                        mIsFaded = Globals.Me.GetSpellCooldown(mSpellBookItem.Id) > Globals.System.GetTimeMs();
+                        mIsFaded = Globals.Me.GetSpellCooldown(mSpellBookItem.Id) > Timing.Global.Milliseconds;
                         if (mIsFaded)
                         {
                             mCooldownLabel.IsHidden = false;
                             var secondsRemaining =
                                 (float) (Globals.Me.GetSpellCooldown(mSpellBookItem.Id) -
-                                         Globals.System.GetTimeMs()) /
+                                         Timing.Global.Milliseconds) /
                                 1000f;
 
                             if (secondsRemaining > 10f)
@@ -463,7 +462,7 @@ namespace Intersect.Client.Interface.Game.Hotbar
                             mCanDrag = true;
                             mMouseX = -1;
                             mMouseY = -1;
-                            if (Globals.System.GetTimeMs() < mClickTime)
+                            if (Timing.Global.Milliseconds < mClickTime)
                             {
                                 Activate();
                                 mClickTime = 0;

Intersect.Client/Maps/WeatherParticle.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using Intersect.Client.Core;
 using Intersect.Client.Entities;
 using Intersect.Client.Framework.GenericClasses;
-using Intersect.Client.Framework.Graphics;
 using Intersect.Client.Framework.Maps;
 using Intersect.Client.General;
 using Intersect.GameObjects;
+using Intersect.Utilities;
 
 namespace Intersect.Client.Maps
 {
@@ -42,7 +42,7 @@ namespace Intersect.Client.Maps
 
         public WeatherParticle(List<IWeatherParticle> RemoveParticle, int xvelocity, int yvelocity, AnimationBase anim)
         {
-            TransmittionTimer = Globals.System.GetTimeMs();
+            TransmittionTimer = Timing.Global.Milliseconds;
             bounds = new Rectangle(0, 0, Graphics.Renderer.GetScreenWidth(), Graphics.Renderer.GetScreenHeight());
 
             xVelocity = xvelocity;
@@ -139,8 +139,8 @@ namespace Intersect.Client.Maps
             }
             else
             {
-                X = originalX + xVelocity * (int)((Globals.System.GetTimeMs() - TransmittionTimer) / 10f);
-                Y = originalY + yVelocity * (int)((Globals.System.GetTimeMs() - TransmittionTimer) / 10f);
+                X = originalX + xVelocity * (int)((Timing.Global.Milliseconds - TransmittionTimer) / 10f);
+                Y = originalY + yVelocity * (int)((Timing.Global.Milliseconds - TransmittionTimer) / 10f);
                 animInstance.SetPosition(cameraSpawnX + X, cameraSpawnY + Y, -1, -1, Guid.Empty, -1, 0);
                 animInstance.Update();
             }

BlestroKain/Broken_Reborn

Intersect.Client/Interface/Game/Hotbar/HotbarItem.cs
@@ -14,6 +14,7 @@ using Intersect.Client.Items;
 using Intersect.Client.Localization;
 using Intersect.Client.Spells;
 using Intersect.GameObjects;
+using Intersect.Utilities;
 
 namespace Intersect.Client.Interface.Game.Hotbar
 {
@@ -136,7 +137,7 @@ namespace Intersect.Client.Interface.Game.Hotbar
 
         void pnl_Clicked(Base sender, ClickedEventArgs arguments)
         {
-            mClickTime = Globals.System.GetTimeMs() + 500;
+            mClickTime = Timing.Global.Milliseconds + 500;
         }
 
         void pnl_HoverLeave(Base sender, EventArgs arguments)
@@ -318,14 +319,14 @@ namespace Intersect.Client.Interface.Game.Hotbar
 
                 //Spell on cd
                 if (mSpellBookItem != null &&
-                    Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) > Globals.System.GetTimeMs())
+                    Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) > Timing.Global.Milliseconds)
                 {
                     updateDisplay = true;
                 }
 
                 //Spell on cd and the fade is incorrect
                 if (mSpellBookItem != null &&
-                    Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) > Globals.System.GetTimeMs() != mIsFaded)
+                    Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) > Timing.Global.Milliseconds != mIsFaded)
                 {
                     updateDisplay = true;
                 }
@@ -392,13 +393,13 @@ namespace Intersect.Client.Interface.Game.Hotbar
                     mCooldownLabel.IsHidden = true;
                     if (mSpellBookItem != null)
                     {
-                        mIsFaded = Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) > Globals.System.GetTimeMs();
+                        mIsFaded = Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) > Timing.Global.Milliseconds;
                         if (mIsFaded)
                         {
                             mCooldownLabel.IsHidden = false;
                             var secondsRemaining =
                                 (float) (Globals.Me.GetSpellCooldown(mSpellBookItem.SpellId) -
-                                         Globals.System.GetTimeMs()) /
+                                         Timing.Global.Milliseconds) /
                                 1000f;
 
                             if (secondsRemaining > 10f)
@@ -474,7 +475,7 @@ namespace Intersect.Client.Interface.Game.Hotbar
                             mCanDrag = true;
                             mMouseX = -1;
                             mMouseY = -1;
-                            if (Globals.System.GetTimeMs() < mClickTime)
+                            if (Timing.Global.Milliseconds < mClickTime)
                             {
                                 Activate();
                                 mClickTime = 0;

Intersect.Client/Maps/WeatherParticle.cs
@@ -6,6 +6,7 @@ using Intersect.Client.Entities;
 using Intersect.Client.Framework.GenericClasses;
 using Intersect.Client.General;
 using Intersect.GameObjects;
+using Intersect.Utilities;
 
 namespace Intersect.Client.Maps
 {
@@ -41,7 +42,7 @@ namespace Intersect.Client.Maps
 
         public WeatherParticle(List<WeatherParticle> RemoveParticle, int xvelocity, int yvelocity, AnimationBase anim)
         {
-            TransmittionTimer = Globals.System.GetTimeMs();
+            TransmittionTimer = Timing.Global.Milliseconds;
             bounds = new Rectangle(0, 0, Graphics.Renderer.GetScreenWidth(), Graphics.Renderer.GetScreenHeight());
 
             xVelocity = xvelocity;
@@ -137,9 +138,9 @@ namespace Intersect.Client.Maps
                 _RemoveParticle.Add(this);
             }
             else
-            {
-                X = originalX + xVelocity * (int) ((Globals.System.GetTimeMs() - TransmittionTimer) / 10f);
-                Y = originalY + yVelocity * (int) ((Globals.System.GetTimeMs() - TransmittionTimer) / 10f);
+            { 
+                X = originalX + xVelocity * (int) ((Timing.Global.Milliseconds - TransmittionTimer) / 10f);
+                Y = originalY + yVelocity * (int) ((Timing.Global.Milliseconds - TransmittionTimer) / 10f);
                 animInstance.SetPosition(cameraSpawnX + X, cameraSpawnY + Y, -1, -1, Guid.Empty, -1, 0);
                 animInstance.Update();
             }
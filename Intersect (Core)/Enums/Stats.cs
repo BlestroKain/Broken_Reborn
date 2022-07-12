2be586a80f2b88f130f5518b5a428e1e32654b8f
Jlrootsloud <63067460+Jlrootsloud@users.noreply.github.com>
2022-07-11 22:23:31 -0500

    Initial parameters
    - Added elemental stats
    - Added bonus stats( cures & Damages)
    - Damage formule Reformed Partially
    - Added New Damages types

--- Intersect (Core)/Enums/Stats.cs
+++ Intersect (Core)/Enums/Stats.cs
@@ -14,6 +14,28 @@ namespace Intersect.Enums
 
         Speed,
 
+        Agility,
+
+        Potency, // porcentual siempre
+
+        Fixeddamage,// Daños fijos
+
+        Cures, // bonus de cura 
+
+        // Elementales
+
+        Fire,
+
+        Water,
+
+        Wind,
+
+        Earth,
+
+        Lightness,
+
+        Darkness,
+
         StatCount
 
     }

26552a9f2f716ea79a0ff67f8eabc0a31ec610c9
Jlrootsloud <109327190+BlestroKain@users.noreply.github.com>
2023-04-02 01:41:50 -0500

    Update Stats.cs

--- Intersect (Core)/Enums/Stats.cs
+++ Intersect (Core)/Enums/Stats.cs
@@ -3,19 +3,15 @@ namespace Intersect.Enums
 
     public enum Stats
     {
-
         Attack = 0,
-
         AbilityPower,
-
         Defense,
-
         MagicResist,
-
         Speed,
-
+        Vitality,
+        Agility,
+        Cures,
+        Potency,
         StatCount
-
     }
-
 }

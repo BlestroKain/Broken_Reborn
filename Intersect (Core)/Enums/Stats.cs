b542ef4a016d161f06ebb9411e4e5bf17af4d2e4
Jlrootsloud <109327190+BlestroKain@users.noreply.github.com>
2023-04-08 21:45:45 -0500

    Revert "Update Stats.cs"
    This reverts commit 871179ed7e0ddde851ba0a30c0c03db1ecf5acaf.

--- Intersect (Core)/Enums/Stats.cs
+++ Intersect (Core)/Enums/Stats.cs
@@ -3,15 +3,19 @@ namespace Intersect.Enums
 
     public enum Stats
     {
+
         Attack = 0,
+
         AbilityPower,
+
         Defense,
+
         MagicResist,
+
         Speed,
-        Vitality,
-        Agility,
-        Cures,
-        Potency,
+
         StatCount
+
     }
+
 }

31bfc38610960149b2dad243e73a12b24675bdd8
BlestroKain <jlrootsloud@gmail.com>
2025-06-17 21:11:52 -0500

    migrations

---
+++ Intersect.Server.Core/Migrations/Sqlite/Game/20250615192529_Addedjobsgamemigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intersect.Server.Migrations.Sqlite.Game
+{
+    /// <inheritdoc />
+    public partial class Addedjobsgamemigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ExperienceAmount",
+                table: "Crafts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Jobs",
+                table: "Crafts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExperienceAmount",
+                table: "Crafts");
+
+            migrationBuilder.DropColumn(
+                name: "Jobs",
+                table: "Crafts");
+        }
+    }
+}

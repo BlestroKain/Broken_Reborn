597c8c8317322cdb4a78da5cd15327e3067ceb49
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-07-25 18:56:05 -0500

    Refactor mail UI components (#40)

--- Intersect.Client.Core/Interface/Game/Mail/MailItem.cs
+++ Intersect.Client.Core/Interface/Game/Mail/MailItem.cs
@@ -1,110 +1,104 @@
-using Intersect.Client.Framework.Gwen.Control.EventArguments;
+using Intersect.Client.Framework.File_Management;
+using Intersect.Client.Framework.Gwen;
 using Intersect.Client.Framework.Gwen.Control;
+using Intersect.Client.Framework.Gwen.Control.EventArguments;
+using Intersect.Client.Framework.Gwen.Input;
+using Intersect.Client.Interface.Game;
 using Intersect.Client.Interface.Game.DescriptionWindows;
 using Intersect.GameObjects;
 using Intersect.Client.General;
 using Intersect.Client.Interface.Game.Mail;
 using Intersect.Client.Items;
-namespace Intersect.Client.Interface.Game.Mail
+namespace Intersect.Client.Interface.Game.Mail;
+
+public partial class MailItem : SlotItem
 {
-    public class MailItem
+    private ItemDescriptionWindow? _descWindow;
+    private readonly SendMailBoxWindow? _sendWindow;
+
+    public Item? CurrentSlot { get; private set; }
+
+    public bool IsEmpty => CurrentSlot == null;
+
+    public MailItem(SendMailBoxWindow sendWindow, Base parent, int index)
+        : base(parent, nameof(MailItem), index, contextMenu: null)
     {
-        public ImagePanel SlotPanel;
-        private ItemDescriptionWindow DescWindow;
+        _sendWindow = sendWindow;
+        Setup();
+    }
 
-        public Item CurrentSlot; // Ranura actual asociada al ítem
-        private int SlotIndex; // Índice del slot en la ventana de correos
-        private MailBoxWindow mMailWindow;
-        private SendMailBoxWindow mSendMailWindow;
-        private MailBoxWindow mailBoxWindow;
-        private int i;
-        private ScrollControl mAttachmentContainer;
+    public MailItem(MailBoxWindow _, Base parent, int index)
+        : base(parent, nameof(MailItem), index, contextMenu: null)
+    {
+        Setup();
+    }
 
-        public bool IsEmpty => CurrentSlot == null;
+    private void Setup()
+    {
+        Icon.HoverEnter += Icon_HoverEnter;
+        Icon.HoverLeave += Icon_HoverLeave;
+        Icon.Clicked += Icon_Clicked;
 
-        public MailItem(SendMailBoxWindow mailWindow, int index, ScrollControl parent)
+        LoadJsonUi(GameContentManager.UI.InGame, Graphics.Renderer.GetResolutionString());
+    }
+
+    public void SetItem(Item slot)
+    {
+        CurrentSlot = slot;
+
+        if (slot != null)
         {
-            mSendMailWindow = mailWindow;
-            SlotIndex = index;
-
-            SlotPanel = new ImagePanel(parent, $"MailSlot{index}");
-            SlotPanel.SetBounds(20 + (index * 60), 180, 32, 32);
-
-            SlotPanel.HoverEnter += Pnl_HoverEnter;
-            SlotPanel.HoverLeave += Pnl_HoverLeave;
-            SlotPanel.RightClicked += Pnl_RightClicked;
-            SlotPanel.Clicked += Pnl_LeftClicked; // Fix for CS1061
-        }
-
-        public MailItem(MailBoxWindow parent, int index, ScrollControl container)
-        {
-            mMailWindow = parent;
-            SlotIndex = index;
-            SlotPanel = new ImagePanel(container, $"AttachmentSlot{index}");
-            SlotPanel.SetBounds(20 + (index * 60), 180, 32, 32);
-            SlotPanel.HoverEnter += Pnl_HoverEnter;
-            SlotPanel.HoverLeave += Pnl_HoverLeave;
-            SlotPanel.RightClicked += Pnl_RightClicked;
-        }
-
-        public void SetItem(Item slot)
-        {
-            CurrentSlot = slot;
-
-            if (slot != null)
+            var itemBase = ItemBase.Get(slot.ItemId);
+            if (itemBase != null)
             {
-                var itemBase = ItemBase.Get(slot.ItemId);
-                if (itemBase != null)
-                {
-                    var texture = Globals.ContentManager.GetTexture(Intersect.Client.Framework.Content.TextureType.Item, itemBase.Icon);
-                    SlotPanel.Texture = texture ?? null;
-                }
-            }
-
-
-            else
-            {
-                ClearItem();
+                var texture = GameContentManager.Current.GetTexture(Framework.Content.TextureType.Item, itemBase.Icon);
+                Icon.Texture = texture;
+                return;
             }
         }
 
-        public void ClearItem()
+        ClearItem();
+    }
+
+    public void ClearItem()
+    {
+        CurrentSlot = null;
+        Icon.Texture = null;
+    }
+
+    private void Icon_HoverEnter(Base sender, EventArgs arguments)
+    {
+        if (CurrentSlot == null)
         {
-            CurrentSlot = null;
-            SlotPanel.Texture = null;
+            return;
         }
 
-        private void Pnl_HoverEnter(Base sender, EventArgs arguments)
+        var itemBase = ItemBase.Get(CurrentSlot.ItemId);
+        if (itemBase == null)
         {
-            if (CurrentSlot != null && ItemBase.Get(CurrentSlot.ItemId) != null)
-            {
-                DescWindow = new ItemDescriptionWindow(
-                    ItemBase.Get(CurrentSlot.ItemId),
-                    CurrentSlot.Quantity,
-                    SlotPanel.X,
-                    SlotPanel.Y,
-                    CurrentSlot.ItemProperties
-                );
-            }
+            return;
         }
 
-        private void Pnl_HoverLeave(Base sender, EventArgs arguments)
-        {
-            if (DescWindow != null)
-            {
-                DescWindow.Dispose();
-                DescWindow = null;
-            }
-        }
+        _descWindow = new ItemDescriptionWindow(
+            itemBase,
+            CurrentSlot.Quantity,
+            Icon.X,
+            Icon.Y,
+            CurrentSlot.ItemProperties
+        );
+    }
 
-        private void Pnl_RightClicked(Base sender, ClickedEventArgs arguments)
-        {
-            
-        }
+    private void Icon_HoverLeave(Base sender, EventArgs arguments)
+    {
+        _descWindow?.Dispose();
+        _descWindow = null;
+    }
 
-        private void Pnl_LeftClicked(Base sender, ClickedEventArgs arguments) // Fix for CS0103
+    private void Icon_Clicked(Base sender, MouseButtonState arguments)
+    {
+        if (arguments.MouseButton is MouseButton.Right && _sendWindow != null)
         {
-         
+            _sendWindow.RemoveAttachment(this);
         }
     }
 }

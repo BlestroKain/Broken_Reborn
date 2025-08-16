9b92e780a2ddfa24d97fec1cab436b9c08bce312
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-08-16 09:36:32 -0500

    feat: add trade chat channel

--- Intersect.Client.Core/Interface/Game/Chat/ChatboxMsg.cs
+++ Intersect.Client.Core/Interface/Game/Chat/ChatboxMsg.cs
@@ -19,9 +19,10 @@ public partial class ChatboxMsg
         { ChatboxTab.Party, new ChatMessageType[] { ChatMessageType.Party, ChatMessageType.PM, ChatMessageType.Admin } },
         { ChatboxTab.Global, new ChatMessageType[] { ChatMessageType.Global, ChatMessageType.PM, ChatMessageType.Admin } },
         { ChatboxTab.Guild, new ChatMessageType[] { ChatMessageType.Guild, ChatMessageType.PM, ChatMessageType.Admin } },
-        { ChatboxTab.System, new ChatMessageType[] { 
-            ChatMessageType.Experience, ChatMessageType.Loot, ChatMessageType.Inventory, ChatMessageType.Bank, 
-            ChatMessageType.Combat, ChatMessageType.Quest, ChatMessageType.Crafting, ChatMessageType.Trading, 
+        { ChatboxTab.Trade, new ChatMessageType[] { ChatMessageType.Trade, ChatMessageType.PM, ChatMessageType.Admin } },
+        { ChatboxTab.System, new ChatMessageType[] {
+            ChatMessageType.Experience, ChatMessageType.Loot, ChatMessageType.Inventory, ChatMessageType.Bank,
+            ChatMessageType.Combat, ChatMessageType.Quest, ChatMessageType.Crafting, ChatMessageType.Trading,
             ChatMessageType.Friend, ChatMessageType.Spells, ChatMessageType.Notice, ChatMessageType.Error,
             ChatMessageType.Admin } },
     };

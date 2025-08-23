7a8372433d9f6a54c8fbacfb648a6123af1beb9d
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-08-23 01:05:46 -0500

    Add effective spell upgrade handling

--- Framework/Intersect.Framework.Core/GameObjects/Spells/SpellCombatDescriptor.cs
+++ Framework/Intersect.Framework.Core/GameObjects/Spells/SpellCombatDescriptor.cs
@@ -3,6 +3,7 @@ using Intersect.Enums;
 using Intersect.Utilities;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Intersect.Framework.Core.GameObjects.Spells;
 
 namespace Intersect.GameObjects;
 
@@ -90,4 +91,133 @@ public partial class SpellCombatDescriptor
 
     [Column("Trap")]
     public int TrapDuration { get; set; }
+
+    private static bool TryGetUpgrade(SpellProperties props, string key, out int value)
+    {
+        value = 0;
+        return props?.CustomUpgrades != null && props.CustomUpgrades.TryGetValue(key, out value);
+    }
+
+    public int GetEffectiveCritChance(SpellProperties props)
+    {
+        var value = CritChance;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.CritChance, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public double GetEffectiveCritMultiplier(SpellProperties props)
+    {
+        var value = CritMultiplier;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.CritMultiplier, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveHitRadius(SpellProperties props)
+    {
+        var value = HitRadius;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.HitRadius, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveCastRange(SpellProperties props)
+    {
+        var value = CastRange;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.CastRange, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveStatDiff(Stat stat, SpellProperties props)
+    {
+        var value = StatDiff[(int)stat];
+        var key = SpellUpgradeKeys.Combat.StatDiff.GetKey(stat);
+        if (key != null && TryGetUpgrade(props, key, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public long GetEffectiveVitalDiff(Vital vital, SpellProperties props)
+    {
+        var value = VitalDiff[(int)vital];
+        var key = SpellUpgradeKeys.Combat.VitalDiff.GetKey(vital);
+        if (key != null && TryGetUpgrade(props, key, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveScaling(SpellProperties props)
+    {
+        var value = Scaling;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.Scaling, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveDuration(SpellProperties props)
+    {
+        var value = Duration;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.Duration, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveHotDotInterval(SpellProperties props)
+    {
+        var value = HotDotInterval;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.HotDotInterval, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveOnHitDuration(SpellProperties props)
+    {
+        var value = OnHitDuration;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.OnHitDuration, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
+
+    public int GetEffectiveTrapDuration(SpellProperties props)
+    {
+        var value = TrapDuration;
+        if (TryGetUpgrade(props, SpellUpgradeKeys.Combat.TrapDuration, out var up))
+        {
+            value += up;
+        }
+
+        return value;
+    }
 }

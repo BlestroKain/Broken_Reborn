03d26124494f2de82def8879587562ba71125fcb
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-08-23 01:00:11 -0500

    Introduce SpellUpgradeKeys registry (#142)

--- Framework/Intersect.Framework.Core/GameObjects/Spells/SpellProperties.cs
+++ Framework/Intersect.Framework.Core/GameObjects/Spells/SpellProperties.cs
@@ -10,12 +10,46 @@ public class SpellProperties
     public SpellProperties(SpellProperties other)
     {
         Level = other.Level;
-        CustomUpgrades = new Dictionary<string, int>(other.CustomUpgrades);
+        CustomUpgrades = new Dictionary<string, int>(
+            other.CustomUpgrades.Where(kv => SpellUpgradeKeys.IsValid(kv.Key))
+        );
     }
 
     [Key(0)]
     public int Level { get; set; } = 1;
 
+    private Dictionary<string, int> _customUpgrades = new();
+
     [Key(1)]
-    public Dictionary<string, int> CustomUpgrades { get; set; } = new();
+    public Dictionary<string, int> CustomUpgrades
+    {
+        get => _customUpgrades;
+        set
+        {
+            _customUpgrades = new Dictionary<string, int>();
+            if (value == null)
+            {
+                return;
+            }
+
+            foreach (var kv in value)
+            {
+                if (SpellUpgradeKeys.IsValid(kv.Key))
+                {
+                    _customUpgrades[kv.Key] = kv.Value;
+                }
+            }
+        }
+    }
+
+    public bool TrySetCustomUpgrade(string key, int value)
+    {
+        if (!SpellUpgradeKeys.IsValid(key))
+        {
+            return false;
+        }
+
+        _customUpgrades[key] = value;
+        return true;
+    }
 }

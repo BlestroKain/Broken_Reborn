f2f5e1ae69b2cc6565d230ff08ff1b01e1f90ed3
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-08-24 15:47:01 -0500

    Add set bonus tier support

--- Intersect.Tests/GameObjects/SetDescriptorTests.cs
+++ Intersect.Tests/GameObjects/SetDescriptorTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Intersect.Enums;
+using Intersect.Framework.Core.GameObjects.Items;
 using Intersect.GameObjects;
 using NUnit.Framework;
 
@@ -12,20 +14,27 @@ public class SetDescriptorTests
     {
         var descriptor = new SetDescriptor(Guid.NewGuid())
         {
-            Stats = new int[1],
-            PercentageStats = new int[1],
-            Vitals = new long[1],
-            VitalsRegen = new long[1],
-            PercentageVitals = new int[1]
+            BonusTiers = new Dictionary<int, SetBonusTier>
+            {
+                [1] = new SetBonusTier
+                {
+                    Stats = new int[1],
+                    PercentageStats = new int[1],
+                    Vitals = new long[1],
+                    VitalsRegen = new long[1],
+                    PercentageVitals = new int[1]
+                }
+            }
         };
 
         descriptor.Validate();
 
-        Assert.That(descriptor.Stats.Length, Is.EqualTo(Enum.GetValues<Stat>().Length));
-        Assert.That(descriptor.PercentageStats.Length, Is.EqualTo(Enum.GetValues<Stat>().Length));
-        Assert.That(descriptor.Vitals.Length, Is.EqualTo(Enum.GetValues<Vital>().Length));
-        Assert.That(descriptor.VitalsRegen.Length, Is.EqualTo(Enum.GetValues<Vital>().Length));
-        Assert.That(descriptor.PercentageVitals.Length, Is.EqualTo(Enum.GetValues<Vital>().Length));
+        var tier = descriptor.BonusTiers[1];
+        Assert.That(tier.Stats.Length, Is.EqualTo(Enum.GetValues<Stat>().Length));
+        Assert.That(tier.PercentageStats.Length, Is.EqualTo(Enum.GetValues<Stat>().Length));
+        Assert.That(tier.Vitals.Length, Is.EqualTo(Enum.GetValues<Vital>().Length));
+        Assert.That(tier.VitalsRegen.Length, Is.EqualTo(Enum.GetValues<Vital>().Length));
+        Assert.That(tier.PercentageVitals.Length, Is.EqualTo(Enum.GetValues<Vital>().Length));
     }
 
     [Test]
@@ -35,31 +44,65 @@ public class SetDescriptorTests
         var vitalLen = Enum.GetValues<Vital>().Length;
         var descriptor = new SetDescriptor(Guid.NewGuid())
         {
-            Stats = new int[statLen + 1],
-            PercentageStats = new int[statLen + 1],
-            Vitals = new long[vitalLen + 1],
-            VitalsRegen = new long[vitalLen + 1],
-            PercentageVitals = new int[vitalLen + 1]
+            BonusTiers = new Dictionary<int, SetBonusTier>
+            {
+                [1] = new SetBonusTier
+                {
+                    Stats = new int[statLen + 1],
+                    PercentageStats = new int[statLen + 1],
+                    Vitals = new long[vitalLen + 1],
+                    VitalsRegen = new long[vitalLen + 1],
+                    PercentageVitals = new int[vitalLen + 1]
+                }
+            }
         };
 
-        descriptor.Stats[^1] = 42;
-        descriptor.PercentageStats[^1] = 42;
-        descriptor.Vitals[^1] = 42;
-        descriptor.VitalsRegen[^1] = 42;
-        descriptor.PercentageVitals[^1] = 42;
+        var tier = descriptor.BonusTiers[1];
+        tier.Stats[^1] = 42;
+        tier.PercentageStats[^1] = 42;
+        tier.Vitals[^1] = 42;
+        tier.VitalsRegen[^1] = 42;
+        tier.PercentageVitals[^1] = 42;
 
         descriptor.Validate();
 
-        Assert.That(descriptor.Stats.Length, Is.EqualTo(statLen));
-        Assert.That(descriptor.PercentageStats.Length, Is.EqualTo(statLen));
-        Assert.That(descriptor.Vitals.Length, Is.EqualTo(vitalLen));
-        Assert.That(descriptor.VitalsRegen.Length, Is.EqualTo(vitalLen));
-        Assert.That(descriptor.PercentageVitals.Length, Is.EqualTo(vitalLen));
+        var validatedTier = descriptor.BonusTiers[1];
+        Assert.That(validatedTier.Stats.Length, Is.EqualTo(statLen));
+        Assert.That(validatedTier.PercentageStats.Length, Is.EqualTo(statLen));
+        Assert.That(validatedTier.Vitals.Length, Is.EqualTo(vitalLen));
+        Assert.That(validatedTier.VitalsRegen.Length, Is.EqualTo(vitalLen));
+        Assert.That(validatedTier.PercentageVitals.Length, Is.EqualTo(vitalLen));
 
-        Assert.That(descriptor.Stats, Does.Not.Contain(42));
-        Assert.That(descriptor.PercentageStats, Does.Not.Contain(42));
-        Assert.That(descriptor.Vitals, Does.Not.Contain(42L));
-        Assert.That(descriptor.VitalsRegen, Does.Not.Contain(42L));
-        Assert.That(descriptor.PercentageVitals, Does.Not.Contain(42));
+        Assert.That(validatedTier.Stats, Does.Not.Contain(42));
+        Assert.That(validatedTier.PercentageStats, Does.Not.Contain(42));
+        Assert.That(validatedTier.Vitals, Does.Not.Contain(42L));
+        Assert.That(validatedTier.VitalsRegen, Does.Not.Contain(42L));
+        Assert.That(validatedTier.PercentageVitals, Does.Not.Contain(42));
+    }
+
+    [Test]
+    public void ValidateMigratesLegacyBonuses()
+    {
+        var descriptor = new SetDescriptor(Guid.NewGuid())
+        {
+            Stats = new[] { 1 },
+            PercentageStats = new[] { 2 },
+            Vitals = new long[] { 3 },
+            VitalsRegen = new long[] { 4 },
+            PercentageVitals = new[] { 5 },
+            Effects = new List<EffectData> { new(ItemEffect.Luck, 10) },
+            ItemIds = new List<Guid> { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }
+        };
+
+        descriptor.Validate();
+
+        Assert.That(descriptor.BonusTiers.ContainsKey(3), Is.True);
+        var tier = descriptor.BonusTiers[3];
+        Assert.That(tier.Stats[0], Is.EqualTo(1));
+        Assert.That(tier.PercentageStats[0], Is.EqualTo(2));
+        Assert.That(tier.Vitals[0], Is.EqualTo(3));
+        Assert.That(tier.VitalsRegen[0], Is.EqualTo(4));
+        Assert.That(tier.PercentageVitals[0], Is.EqualTo(5));
+        Assert.That(tier.Effects[0].Percentage, Is.EqualTo(10));
     }
 }

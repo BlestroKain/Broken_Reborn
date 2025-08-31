2d415f1ab2a2fb4ac81ff511976426b760021f4a
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-08-30 22:06:04 -0500

    feat: add prism visuals

---
+++ Intersect.Client.Core/Maps/Prisms/PrismVisualManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intersect.Client.Framework.Graphics;
+using Intersect.Client.General;
+using Intersect.Client.Maps;
+using Intersect.Client.Networking;
+using Intersect.Config;
+using Intersect.Network.Packets.Server;
+
+namespace Intersect.Client.Maps.Prisms;
+
+public static class PrismVisualManager
+{
+    private static readonly Dictionary<Guid, PrismVisual> Prisms = new();
+
+    public static void Synchronize(PrismListPacket packet)
+    {
+        Prisms.Clear();
+        foreach (var prism in packet.Prisms)
+        {
+            Update(prism);
+        }
+    }
+
+    public static void Update(PrismUpdatePacket packet)
+    {
+        if (!Prisms.TryGetValue(packet.MapId, out var visual))
+        {
+            var info = PrismConfig.Prisms.FirstOrDefault(p => p.MapId == packet.MapId);
+            if (info == null)
+            {
+                return;
+            }
+
+            visual = new PrismVisual(info);
+            Prisms[packet.MapId] = visual;
+        }
+
+        visual.Update(packet.Owner, packet.State, packet.Hp, packet.MaxHp);
+    }
+
+    public static void Draw(MapInstance map)
+    {
+        if (Prisms.TryGetValue(map.Id, out var visual))
+        {
+            visual.Draw(map);
+        }
+    }
+
+    public static bool TryClickAttack()
+    {
+        var mouse = Graphics.ConvertToWorldPoint(Globals.InputManager.GetMousePosition());
+        var x = (int)mouse.X;
+        var y = (int)mouse.Y;
+
+        foreach (var visual in Prisms.Values)
+        {
+            if (visual.HitTest(x, y))
+            {
+                PacketSender.SendPrismAttack(visual.MapId);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

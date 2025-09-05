7fd2e25aea5f637c38606c6561eb4a408acfcf17
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-09-04 21:13:03 -0500

    Add designer assets for zone editor (#365)

---
+++ Intersect.Editor/Forms/Editors/frmZoneEditor.Designer.cs
@@ -0,0 +1,255 @@
+using DarkUI.Controls;
+
+namespace Intersect.Editor.Forms.Editors
+{
+    partial class FrmZoneEditor
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmZoneEditor));
+            pnlContainer = new System.Windows.Forms.Panel();
+            treeZones = new System.Windows.Forms.TreeView();
+            chkOverrideFlags = new System.Windows.Forms.CheckBox();
+            grpFlags = new DarkGroupBox();
+            chkOverrideModifiers = new System.Windows.Forms.CheckBox();
+            grpModifiers = new DarkGroupBox();
+            btnSave = new DarkButton();
+            btnCancel = new DarkButton();
+            toolStrip = new DarkToolStrip();
+            toolStripItemNew = new System.Windows.Forms.ToolStripButton();
+            toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            toolStripItemDelete = new System.Windows.Forms.ToolStripButton();
+            toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            btnAlphabetical = new System.Windows.Forms.ToolStripButton();
+            toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            toolStripItemCopy = new System.Windows.Forms.ToolStripButton();
+            toolStripItemPaste = new System.Windows.Forms.ToolStripButton();
+            toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            toolStripItemUndo = new System.Windows.Forms.ToolStripButton();
+            pnlContainer.SuspendLayout();
+            toolStrip.SuspendLayout();
+            SuspendLayout();
+            //
+            // pnlContainer
+            //
+            pnlContainer.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            pnlContainer.Controls.Add(btnCancel);
+            pnlContainer.Controls.Add(btnSave);
+            pnlContainer.Controls.Add(grpModifiers);
+            pnlContainer.Controls.Add(chkOverrideModifiers);
+            pnlContainer.Controls.Add(grpFlags);
+            pnlContainer.Controls.Add(chkOverrideFlags);
+            pnlContainer.Controls.Add(treeZones);
+            pnlContainer.Location = new System.Drawing.Point(0, 29);
+            pnlContainer.Name = "pnlContainer";
+            pnlContainer.Size = new System.Drawing.Size(484, 392);
+            pnlContainer.TabIndex = 1;
+            //
+            // treeZones
+            //
+            treeZones.BackColor = System.Drawing.Color.FromArgb(60, 63, 65);
+            treeZones.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            treeZones.Dock = System.Windows.Forms.DockStyle.Left;
+            treeZones.ForeColor = System.Drawing.Color.Gainsboro;
+            treeZones.Location = new System.Drawing.Point(0, 0);
+            treeZones.Name = "treeZones";
+            treeZones.Size = new System.Drawing.Size(200, 392);
+            treeZones.TabIndex = 0;
+            //
+            // chkOverrideFlags
+            //
+            chkOverrideFlags.AutoSize = true;
+            chkOverrideFlags.Location = new System.Drawing.Point(210, 10);
+            chkOverrideFlags.Name = "chkOverrideFlags";
+            chkOverrideFlags.Size = new System.Drawing.Size(103, 19);
+            chkOverrideFlags.TabIndex = 1;
+            chkOverrideFlags.Text = "Override Flags";
+            chkOverrideFlags.UseVisualStyleBackColor = true;
+            //
+            // grpFlags
+            //
+            grpFlags.Location = new System.Drawing.Point(210, 35);
+            grpFlags.Name = "grpFlags";
+            grpFlags.Size = new System.Drawing.Size(260, 150);
+            grpFlags.TabIndex = 2;
+            grpFlags.TabStop = false;
+            grpFlags.Text = "Flags";
+            //
+            // chkOverrideModifiers
+            //
+            chkOverrideModifiers.AutoSize = true;
+            chkOverrideModifiers.Location = new System.Drawing.Point(210, 190);
+            chkOverrideModifiers.Name = "chkOverrideModifiers";
+            chkOverrideModifiers.Size = new System.Drawing.Size(127, 19);
+            chkOverrideModifiers.TabIndex = 3;
+            chkOverrideModifiers.Text = "Override Modifiers";
+            chkOverrideModifiers.UseVisualStyleBackColor = true;
+            //
+            // grpModifiers
+            //
+            grpModifiers.Location = new System.Drawing.Point(210, 215);
+            grpModifiers.Name = "grpModifiers";
+            grpModifiers.Size = new System.Drawing.Size(260, 120);
+            grpModifiers.TabIndex = 4;
+            grpModifiers.TabStop = false;
+            grpModifiers.Text = "Modifiers";
+            //
+            // btnSave
+            //
+            btnSave.Location = new System.Drawing.Point(210, 350);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(80, 23);
+            btnSave.TabIndex = 5;
+            btnSave.Text = "Save";
+            //
+            // btnCancel
+            //
+            btnCancel.Location = new System.Drawing.Point(300, 350);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new System.Drawing.Size(80, 23);
+            btnCancel.TabIndex = 6;
+            btnCancel.Text = "Cancel";
+            //
+            // toolStrip
+            //
+            toolStrip.AutoSize = false;
+            toolStrip.BackColor = System.Drawing.Color.FromArgb(45, 45, 48);
+            toolStrip.ForeColor = System.Drawing.Color.FromArgb(220, 220, 220);
+            toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripItemNew, toolStripSeparator1, toolStripItemDelete, toolStripSeparator2, btnAlphabetical, toolStripSeparator4, toolStripItemCopy, toolStripItemPaste, toolStripSeparator3, toolStripItemUndo });
+            toolStrip.Location = new System.Drawing.Point(0, 0);
+            toolStrip.Name = "toolStrip";
+            toolStrip.Padding = new System.Windows.Forms.Padding(6, 0, 1, 0);
+            toolStrip.Size = new System.Drawing.Size(484, 29);
+            toolStrip.TabIndex = 0;
+            toolStrip.Text = "toolStrip1";
+            //
+            // toolStripItemNew
+            //
+            toolStripItemNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            toolStripItemNew.Enabled = false;
+            toolStripItemNew.Image = (System.Drawing.Image)resources.GetObject("toolStripItemNew.Image");
+            toolStripItemNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            toolStripItemNew.Name = "toolStripItemNew";
+            toolStripItemNew.Size = new System.Drawing.Size(23, 26);
+            toolStripItemNew.Text = "New";
+            //
+            // toolStripSeparator1
+            //
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new System.Drawing.Size(6, 29);
+            //
+            // toolStripItemDelete
+            //
+            toolStripItemDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            toolStripItemDelete.Enabled = false;
+            toolStripItemDelete.Image = (System.Drawing.Image)resources.GetObject("toolStripItemDelete.Image");
+            toolStripItemDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            toolStripItemDelete.Name = "toolStripItemDelete";
+            toolStripItemDelete.Size = new System.Drawing.Size(23, 26);
+            toolStripItemDelete.Text = "Delete";
+            //
+            // toolStripSeparator2
+            //
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new System.Drawing.Size(6, 29);
+            //
+            // btnAlphabetical
+            //
+            btnAlphabetical.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            btnAlphabetical.Enabled = false;
+            btnAlphabetical.Image = (System.Drawing.Image)resources.GetObject("btnAlphabetical.Image");
+            btnAlphabetical.ImageTransparentColor = System.Drawing.Color.Magenta;
+            btnAlphabetical.Name = "btnAlphabetical";
+            btnAlphabetical.Size = new System.Drawing.Size(23, 26);
+            btnAlphabetical.Text = "Alphabetical";
+            //
+            // toolStripSeparator4
+            //
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new System.Drawing.Size(6, 29);
+            //
+            // toolStripItemCopy
+            //
+            toolStripItemCopy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            toolStripItemCopy.Enabled = false;
+            toolStripItemCopy.Image = (System.Drawing.Image)resources.GetObject("toolStripItemCopy.Image");
+            toolStripItemCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            toolStripItemCopy.Name = "toolStripItemCopy";
+            toolStripItemCopy.Size = new System.Drawing.Size(23, 26);
+            toolStripItemCopy.Text = "Copy";
+            //
+            // toolStripItemPaste
+            //
+            toolStripItemPaste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            toolStripItemPaste.Enabled = false;
+            toolStripItemPaste.Image = (System.Drawing.Image)resources.GetObject("toolStripItemPaste.Image");
+            toolStripItemPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            toolStripItemPaste.Name = "toolStripItemPaste";
+            toolStripItemPaste.Size = new System.Drawing.Size(23, 26);
+            toolStripItemPaste.Text = "Paste";
+            //
+            // toolStripSeparator3
+            //
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new System.Drawing.Size(6, 29);
+            //
+            // toolStripItemUndo
+            //
+            toolStripItemUndo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            toolStripItemUndo.Enabled = false;
+            toolStripItemUndo.Image = (System.Drawing.Image)resources.GetObject("toolStripItemUndo.Image");
+            toolStripItemUndo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            toolStripItemUndo.Name = "toolStripItemUndo";
+            toolStripItemUndo.Size = new System.Drawing.Size(23, 26);
+            toolStripItemUndo.Text = "Undo";
+            //
+            // FrmZoneEditor
+            //
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(484, 421);
+            Controls.Add(pnlContainer);
+            Controls.Add(toolStrip);
+            Name = "FrmZoneEditor";
+            Text = "Zone Editor";
+            pnlContainer.ResumeLayout(false);
+            pnlContainer.PerformLayout();
+            toolStrip.ResumeLayout(false);
+            toolStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private System.Windows.Forms.Panel pnlContainer;
+        private System.Windows.Forms.TreeView treeZones;
+        private System.Windows.Forms.CheckBox chkOverrideFlags;
+        private DarkGroupBox grpFlags;
+        private System.Windows.Forms.CheckBox chkOverrideModifiers;
+        private DarkGroupBox grpModifiers;
+        private DarkButton btnSave;
+        private DarkButton btnCancel;
+        private DarkToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripItemNew;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripItemDelete;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnAlphabetical;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton toolStripItemCopy;
+        private System.Windows.Forms.ToolStripButton toolStripItemPaste;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolStripItemUndo;
+    }
+}
--- Intersect.Editor/Forms/Editors/frmZoneEditor.cs
+++ Intersect.Editor/Forms/Editors/frmZoneEditor.cs
@@ -2,23 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using DarkUI.Controls;
 using Intersect.Framework.Core.GameObjects.Zones;
 using Intersect.Editor.Networking;
 using Intersect.Enums;
 
 namespace Intersect.Editor.Forms.Editors;
 
-public class FrmZoneEditor : EditorForm
+public partial class FrmZoneEditor : EditorForm
 {
-    private readonly TreeView _tree;
-    private readonly DarkGroupBox _grpFlags;
-    private readonly DarkGroupBox _grpModifiers;
-    private readonly DarkButton btnSave;
-    private readonly DarkButton btnCancel;
-    private readonly CheckBox _chkOverrideFlags;
-    private readonly CheckBox _chkOverrideModifiers;
-
     private readonly Dictionary<ZoneFlags, CheckBox> _flagBoxes = new();
     private readonly Dictionary<string, NumericUpDown> _modifierControls = new();
 
@@ -29,74 +20,66 @@ public class FrmZoneEditor : EditorForm
     public FrmZoneEditor()
     {
         ApplyHooks();
-        _tree = new TreeView { Dock = DockStyle.Left, Width = 200 };
-        _tree.AfterSelect += TreeAfterSelect;
+        InitializeComponent();
+        Icon = Program.Icon;
 
-        _grpFlags = new DarkGroupBox { Text = "Flags", Left = 210, Top = 35, Width = 260, Height = 150 };
-        _grpModifiers = new DarkGroupBox { Text = "Modifiers", Left = 210, Top = 215, Width = 260, Height = 120 };
+        _btnSave = btnSave;
+        _btnCancel = btnCancel;
 
-        _chkOverrideFlags = new CheckBox { Text = "Override Flags", Left = 210, Top = 10, AutoSize = true };
-        _chkOverrideFlags.CheckedChanged += OverrideFlagsChanged;
-        _chkOverrideModifiers = new CheckBox { Text = "Override Modifiers", Left = 210, Top = 190, AutoSize = true };
-        _chkOverrideModifiers.CheckedChanged += OverrideModifiersChanged;
-
-        var y = 20;
-        foreach (ZoneFlags flag in Enum.GetValues(typeof(ZoneFlags)))
-        {
-            if (flag == ZoneFlags.None) continue;
-            var cb = new CheckBox { Text = flag.ToString(), Left = 5, Top = y, AutoSize = true };
-            _grpFlags.Controls.Add(cb);
-            _flagBoxes.Add(flag, cb);
-            y += 25;
-        }
-
-        y = 20;
-        _grpModifiers.Controls.Add(new Label { Text = "Experience Rate", Left = 5, Top = y, AutoSize = true });
-        var numExp = new NumericUpDown { Minimum = 0, Maximum = 1000, Left = 120, Top = y - 3 };
-        _grpModifiers.Controls.Add(numExp);
-        _modifierControls["ExperienceRate"] = numExp;
-        y += 25;
-        _grpModifiers.Controls.Add(new Label { Text = "Gold Rate", Left = 5, Top = y, AutoSize = true });
-        var numGold = new NumericUpDown { Minimum = 0, Maximum = 1000, Left = 120, Top = y - 3 };
-        _grpModifiers.Controls.Add(numGold);
-        _modifierControls["GoldRate"] = numGold;
-
-        btnSave = new DarkButton { Text = "Save", Left = 210, Width = 80, Top = 350 };
+        treeZones.AfterSelect += TreeAfterSelect;
+        chkOverrideFlags.CheckedChanged += OverrideFlagsChanged;
+        chkOverrideModifiers.CheckedChanged += OverrideModifiersChanged;
         btnSave.Click += SaveClick;
-        btnCancel = new DarkButton { Text = "Cancel", Left = 300, Width = 80, Top = 350 };
         btnCancel.Click += CancelClick;
 
-        Controls.AddRange(new Control[]
-        {
-            _tree,
-            _chkOverrideFlags,
-            _chkOverrideModifiers,
-            _grpFlags,
-            _grpModifiers,
-            btnSave,
-            btnCancel
-        });
-
-        Icon = Program.Icon;
-        Text = "Zone Editor";
-        Width = 500;
-        Height = 420;
-
-        btnSave.Visible = false;
-        btnCancel.Visible = false;
-        btnSave.Enabled = false;
-        btnCancel.Enabled = false;
-
-        _btnSave = btnSave; // assign to base class
-        _btnCancel = btnCancel;
+        SetupFlagControls();
+        SetupModifierControls();
 
         PopulateTree();
         UpdateEditorButtons(false);
     }
 
+    private void SetupFlagControls()
+    {
+        var y = 20;
+        foreach (ZoneFlags flag in Enum.GetValues(typeof(ZoneFlags)))
+        {
+            if (flag == ZoneFlags.None)
+            {
+                continue;
+            }
+
+            var cb = new CheckBox
+            {
+                Text = flag.ToString(),
+                Left = 5,
+                Top = y,
+                AutoSize = true
+            };
+            grpFlags.Controls.Add(cb);
+            _flagBoxes.Add(flag, cb);
+            y += 25;
+        }
+    }
+
+    private void SetupModifierControls()
+    {
+        var y = 20;
+        grpModifiers.Controls.Add(new Label { Text = "Experience Rate", Left = 5, Top = y, AutoSize = true });
+        var numExp = new NumericUpDown { Minimum = 0, Maximum = 1000, Left = 120, Top = y - 3 };
+        grpModifiers.Controls.Add(numExp);
+        _modifierControls["ExperienceRate"] = numExp;
+        y += 25;
+
+        grpModifiers.Controls.Add(new Label { Text = "Gold Rate", Left = 5, Top = y, AutoSize = true });
+        var numGold = new NumericUpDown { Minimum = 0, Maximum = 1000, Left = 120, Top = y - 3 };
+        grpModifiers.Controls.Add(numGold);
+        _modifierControls["GoldRate"] = numGold;
+    }
+
     private void PopulateTree()
     {
-        _tree.Nodes.Clear();
+        treeZones.Nodes.Clear();
         foreach (var pair in Zone.Lookup.OrderBy(p => p.Value?.Name))
         {
             var zoneNode = new TreeNode(pair.Value?.Name ?? string.Empty) { Tag = pair.Value };
@@ -104,7 +87,7 @@ public class FrmZoneEditor : EditorForm
             {
                 zoneNode.Nodes.Add(new TreeNode(sub.Value?.Name ?? string.Empty) { Tag = sub.Value });
             }
-            _tree.Nodes.Add(zoneNode);
+            treeZones.Nodes.Add(zoneNode);
         }
     }
 
@@ -130,8 +113,8 @@ public class FrmZoneEditor : EditorForm
         {
             foreach (var cb in _flagBoxes.Values) cb.Enabled = false;
             foreach (var num in _modifierControls.Values) num.Enabled = false;
-            _chkOverrideFlags.Visible = false;
-            _chkOverrideModifiers.Visible = false;
+            chkOverrideFlags.Visible = false;
+            chkOverrideModifiers.Visible = false;
             return;
         }
 
@@ -141,10 +124,10 @@ public class FrmZoneEditor : EditorForm
         {
             flags = _currentSubzone.Flags ?? _currentZone.Flags;
             modifiers = _currentSubzone.Modifiers ?? _currentZone.Modifiers;
-            _chkOverrideFlags.Visible = true;
-            _chkOverrideModifiers.Visible = true;
-            _chkOverrideFlags.Checked = _currentSubzone.Flags.HasValue;
-            _chkOverrideModifiers.Checked = _currentSubzone.Modifiers != null;
+            chkOverrideFlags.Visible = true;
+            chkOverrideModifiers.Visible = true;
+            chkOverrideFlags.Checked = _currentSubzone.Flags.HasValue;
+            chkOverrideModifiers.Checked = _currentSubzone.Modifiers != null;
             foreach (var cb in _flagBoxes.Values)
             {
                 cb.Enabled = _currentSubzone.Flags.HasValue;
@@ -158,8 +141,8 @@ public class FrmZoneEditor : EditorForm
         {
             flags = _currentZone.Flags;
             modifiers = _currentZone.Modifiers;
-            _chkOverrideFlags.Visible = false;
-            _chkOverrideModifiers.Visible = false;
+            chkOverrideFlags.Visible = false;
+            chkOverrideModifiers.Visible = false;
             foreach (var cb in _flagBoxes.Values)
             {
                 cb.Enabled = true;
@@ -182,14 +165,14 @@ public class FrmZoneEditor : EditorForm
     private void OverrideFlagsChanged(object? sender, EventArgs e)
     {
         if (_currentSubzone == null || _loading) return;
-        _currentSubzone.Flags = _chkOverrideFlags.Checked ? _currentZone?.Flags ?? ZoneFlags.None : null;
+        _currentSubzone.Flags = chkOverrideFlags.Checked ? _currentZone?.Flags ?? ZoneFlags.None : null;
         LoadValues();
     }
 
     private void OverrideModifiersChanged(object? sender, EventArgs e)
     {
         if (_currentSubzone == null || _loading) return;
-        _currentSubzone.Modifiers = _chkOverrideModifiers.Checked ? new ZoneModifiers() : null;
+        _currentSubzone.Modifiers = chkOverrideModifiers.Checked ? new ZoneModifiers() : null;
         LoadValues();
     }
 

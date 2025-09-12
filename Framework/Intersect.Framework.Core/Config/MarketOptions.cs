5f0d09a6ebcf764239e42e214664929a43061abd
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-09-11 21:17:40 -0500

    Add MarketOptions to configure page size

---
+++ Framework/Intersect.Framework.Core/Config/MarketOptions.cs
@@ -0,0 +1,10 @@
+namespace Intersect.Config;
+
+public partial class MarketOptions
+{
+    /// <summary>
+    /// Number of listings to display per page in market searches.
+    /// </summary>
+    public int PageSize { get; set; } = 10;
+}
+

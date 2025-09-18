3da86ea56236effe4eb15fc6ab2603ede92b10af
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-09-18 07:16:25 -0500

    Add pet state enum and initial spawn pet command (#520)

---
+++ Framework/Intersect.Framework.Core/GameObjects/Events/Commands/SpawnPetCommand.cs
@@ -0,0 +1,23 @@
+using Intersect.Enums;
+
+namespace Intersect.Framework.Core.GameObjects.Events.Commands;
+
+public partial class SpawnPetCommand : EventCommand
+{
+    public override EventCommandType Type { get; } = EventCommandType.Null;
+
+    public Guid PetId { get; set; }
+
+    public Direction Dir { get; set; }
+
+    //Tile Spawn Variables  (Will spawn on map tile if mapid is not empty)
+    public Guid MapId { get; set; }
+
+    //Entity Spawn Variables (Will spawn on/around entity if entityId is not empty)
+    public Guid EntityId { get; set; }
+
+    //Map Coords or Coords Centered around player to spawn at
+    public sbyte X { get; set; }
+
+    public sbyte Y { get; set; }
+}

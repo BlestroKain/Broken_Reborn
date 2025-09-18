0cbc1300d292f0e16cfc35cbf1a9ef82dc07adb8
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-09-18 12:04:11 -0500

    Add SpawnPet event command type (#522)

--- Framework/Intersect.Framework.Core/GameObjects/Events/Commands/SpawnPetCommand.cs
+++ Framework/Intersect.Framework.Core/GameObjects/Events/Commands/SpawnPetCommand.cs
@@ -4,19 +4,19 @@ namespace Intersect.Framework.Core.GameObjects.Events.Commands;
 
 public partial class SpawnPetCommand : EventCommand
 {
-    public override EventCommandType Type { get; } = EventCommandType.Null;
+    public override EventCommandType Type { get; } = EventCommandType.SpawnPet;
 
     public Guid PetId { get; set; }
 
     public Direction Dir { get; set; }
 
-    //Tile Spawn Variables  (Will spawn on map tile if mapid is not empty)
+    // Pet tile spawn variables (spawns on the specified map tile when MapId is set)
     public Guid MapId { get; set; }
 
-    //Entity Spawn Variables (Will spawn on/around entity if entityId is not empty)
+    // Pet entity spawn variables (spawns relative to the specified entity when EntityId is set)
     public Guid EntityId { get; set; }
 
-    //Map Coords or Coords Centered around player to spawn at
+    // Map coordinates or offsets relative to the pet spawn target
     public sbyte X { get; set; }
 
     public sbyte Y { get; set; }

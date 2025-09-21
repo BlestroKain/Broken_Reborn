d54cb3ff735e6f6129c836e3398b68578b5f2346
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-09-21 16:28:16 -0500

    Persist pet progress between spawns (#591)

--- Intersect.Server.Core/Database/PlayerData/Players/PlayerPet.cs
+++ Intersect.Server.Core/Database/PlayerData/Players/PlayerPet.cs
@@ -1,6 +1,8 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using Intersect.Enums;
 using Intersect.Framework.Core.GameObjects.Pets;
+using Intersect.Server.Database;
 using Intersect.Server.Entities;
 using Newtonsoft.Json;
 
@@ -27,7 +29,53 @@ public partial class PlayerPet : IPlayerOwned
 
     public long Experience { get; set; }
 
+    public int StatPoints { get; set; }
+
+    [NotMapped]
+    public int[] BaseStats { get; set; } = new int[Enum.GetValues<Stat>().Length];
+
+    [NotMapped]
+    public int[] StatPointAllocations { get; set; } = new int[Enum.GetValues<Stat>().Length];
+
+    [NotMapped]
+    public long[] MaxVitals { get; set; } = new long[Enum.GetValues<Vital>().Length];
+
+    [NotMapped]
+    public long[] Vitals { get; set; } = new long[Enum.GetValues<Vital>().Length];
+
     public long TimeCreated { get; set; } = DateTime.UtcNow.ToBinary();
+
+    [JsonIgnore]
+    [Column("PetBaseStats")]
+    public string BaseStatsJson
+    {
+        get => DatabaseUtils.SaveIntArray(BaseStats, Enum.GetValues<Stat>().Length);
+        set => BaseStats = DatabaseUtils.LoadIntArray(value, Enum.GetValues<Stat>().Length);
+    }
+
+    [JsonIgnore]
+    [Column("PetStatPointAllocations")]
+    public string StatPointAllocationsJson
+    {
+        get => DatabaseUtils.SaveIntArray(StatPointAllocations, Enum.GetValues<Stat>().Length);
+        set => StatPointAllocations = DatabaseUtils.LoadIntArray(value, Enum.GetValues<Stat>().Length);
+    }
+
+    [JsonIgnore]
+    [Column("PetMaxVitals")]
+    public string MaxVitalsJson
+    {
+        get => DatabaseUtils.SaveLongArray(MaxVitals, Enum.GetValues<Vital>().Length);
+        set => MaxVitals = DatabaseUtils.LoadLongArray(value, Enum.GetValues<Vital>().Length);
+    }
+
+    [JsonIgnore]
+    [Column("PetVitals")]
+    public string VitalsJson
+    {
+        get => DatabaseUtils.SaveLongArray(Vitals, Enum.GetValues<Vital>().Length);
+        set => Vitals = DatabaseUtils.LoadLongArray(value, Enum.GetValues<Vital>().Length);
+    }
 
     [NotMapped, JsonIgnore]
     public PetDescriptor? Descriptor
